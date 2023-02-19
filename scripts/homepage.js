025b1f2c6bfe91e2da24102c649bee23911b809c
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2023-02-19 21:40:06 +0700

    script to store user selections

---
+++ scripts/homepage.js
@@ -0,0 +1,22 @@
+let topics = document.getElementById("topicSelect")
+let amount = document.getElementById("amountSelect")
+let difficulty = document.getElementById("difficultySelect")
+let generate_btn = document.getElementById("generate")
+
+let selected_topic = ""
+let selected_difficulty = ""
+let selected_amount = 0
+
+//Generate button on click
+generate_btn.addEventListener('click', () => {
+    //Store the user selection of topic
+    selected_topic = topics.value
+    //Store the number of questions selected
+    selected_amount = amount.value
+    //Store the user selection of difficulty
+    selected_difficulty = difficulty.value
+    console.log(selected_amount)
+}
+)
+
+
