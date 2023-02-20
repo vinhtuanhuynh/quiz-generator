6ee8a15d0c6d2cd7d81da58f7f0fd5133af026d3
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2023-02-20 11:34:07 +0700

    load the question and shuffle the answers

--- scripts/question.js
+++ scripts/question.js
@@ -1,13 +1,15 @@
 const api_url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
+const randGeneratedAr = []
+const randGeneratedAr2 = []
 
 let current_question = 0;
-let score = 0;
 let questionList = "";
 let question_text = document.getElementById('question')
 let a_text = document.getElementById('a_text')
 let b_text = document.getElementById('b_text')
 let c_text = document.getElementById('c_text')
 let d_text = document.getElementById('d_text')
+let answer_texts = [a_text, b_text, c_text, d_text]
 
 async function getQuestions() {
     // Making an API call (request)and getting the response back
@@ -15,6 +17,37 @@ async function getQuestions() {
     // Parsing it to JSON format
     const data = await response.json();
     questionList = data.results
+    //After we get the response from the API, load the question
+    loadQuestion()
+}
+
+//function to load the question from the API into the created html
+function loadQuestion() {
+    let current_question_data = questionList[current_question]
+    question_text.innerText = current_question_data.question
+    let answer_choices = [
+        current_question_data.correct_answer, 
+        current_question_data.incorrect_answers[0],
+        current_question_data.incorrect_answers[1],
+        current_question_data.incorrect_answers[2],
+    ]
+    shuffleAnswers(answer_choices)
+}
+
+//function to assign an answer from the API with a random letter (a, b, c, d)
+function shuffleAnswers(answer_choices) {
+    let amount = answer_choices.length
+    while (amount) {
+        let answerTextIndex = Math.floor(Math.random() * 4);
+        let answerChoiceIndex = Math.floor(Math.random() * 4);
+        if(randGeneratedAr.includes(answerTextIndex) || randGeneratedAr2.includes(answerChoiceIndex)){
+            continue;
+        }
+        randGeneratedAr.push(answerTextIndex);
+        randGeneratedAr2.push(answerChoiceIndex);
+        answer_texts[answerTextIndex].innerText = answer_choices[answerChoiceIndex];
+        amount--;
+    }
 }
 
 getQuestions()
