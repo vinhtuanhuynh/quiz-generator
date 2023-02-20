2ce002b89b384320f5da287281ba0e612378545f
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2023-02-20 13:42:32 +0700

    get the id of the selected answer

--- scripts/question.js
+++ scripts/question.js
@@ -11,6 +11,7 @@ let c_text = document.getElementById('c_text')
 let d_text = document.getElementById('d_text')
 let answers_list = document.querySelectorAll('.answer')
 let answer_texts = [a_text, b_text, c_text, d_text]
+let submit_btn = document.getElementById('submit')
 
 async function getQuestions() {
     // Making an API call (request)and getting the response back
@@ -19,15 +20,15 @@ async function getQuestions() {
     const data = await response.json();
     questionList = data.results
     //After we get the response from the API, load the question
-    loadQuestion()
+    loadQuestion();
 }
 
 //function to load the question from the API into the created html
 function loadQuestion() {
-    let current_question_data = questionList[current_question]
-    question_text.innerText = current_question_data.question
+    let current_question_data = questionList[current_question];
+    question_text.innerText = current_question_data.question;
     let answer_choices = [
-        current_question_data.correct_answer, 
+        current_question_data.correct_answer,
         current_question_data.incorrect_answers[0],
         current_question_data.incorrect_answers[1],
         current_question_data.incorrect_answers[2],
@@ -37,11 +38,11 @@ function loadQuestion() {
 
 //function to assign an answer from the API with a random letter (a, b, c, d)
 function shuffleAnswers(answer_choices) {
-    let amount = answer_choices.length
+    let amount = answer_choices.length;
     while (amount) {
         let answerTextIndex = Math.floor(Math.random() * 4);
         let answerChoiceIndex = Math.floor(Math.random() * 4);
-        if(randGeneratedAr.includes(answerTextIndex) || randGeneratedAr2.includes(answerChoiceIndex)){
+        if (randGeneratedAr.includes(answerTextIndex) || randGeneratedAr2.includes(answerChoiceIndex)) {
             continue;
         }
         randGeneratedAr.push(answerTextIndex);
@@ -55,8 +56,20 @@ function shuffleAnswers(answer_choices) {
 function deSelectAnswers() {
     //uncheck all option
     answers_list.forEach(option => {
-        option.checked = false
+        option.checked = false;
     })
+}
+
+//get the answer id the user selected
+function getSelected() {
+    let answer_id = '';
+    answers_list.forEach((option) => {
+        if (option.checked) {
+            //id is a, b, c or d
+            answer_id = option.id;
+        }
+    })
+    return answer_id
 }
 
 getQuestions()
